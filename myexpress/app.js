9b03a4986cef2799c52f38eac6fc623107602324
sandeepkaranam1 <sandeepkaranam@rocketmail.com>
2018-03-24 03:29:11 -0400

    gvb

--- myexpress/app.js
+++ myexpress/app.js
@@ -1,6 +1,7 @@
 var express=require('express');
 var bodyParser=require('body-parser');
 var path=require('path');
+//var pug=require('pug');
 
 var port=3000;
 
@@ -11,16 +12,28 @@ console.log('time:',Date.now());
 next();
 });
 
+//app.set('view engine','pug');
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'))
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',function(req,res){
-	res.send('Hello World');
+	res.render('index',
+		{title:'Hello World',
+			showTitle:false,
+			people:['sandeep','Karanam','Raj']
+	});
+	
 });
 
 app.get('/about',function(req,res){
-	res.send('Its about About page');
+	res.render('about');
+});
+app.get('/contact',function(req,res){
+	res.render('contact');
 });
 app.listen(port);
 console.log('Server Started on Port '+ port);
